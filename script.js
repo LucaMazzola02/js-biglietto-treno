d16590fdd3653f5b4ac59815bec050c59dbecdda
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-05-12 17:01:53 +0200

    create the train ticket cicle

---
+++ script.js
@@ -0,0 +1,33 @@
+let chilometri = parseInt(prompt("Quanti chilometri deve percorrere?"));
+
+let age=parseInt(prompt("Quanti anni ha?"));
+
+//calcolo: let prezzo = ( 0.21 * km)
+
+let prezzo = chilometri * 0.21;
+
+ //per la percentuale basta fare *0.20 o *0.40 :DD
+
+    let spesa;
+
+if (age < 18) {
+
+    spesa = prezzo * 0.80; //sconto del 20%
+    
+} else if (age > 65) {
+
+    spesa = prezzo * 0.60; //sconto del 40%
+    
+} else {
+
+    spesa = prezzo;  //prezzo intero
+
+}
+
+
+// converto il prezzo con massimo dues decimali
+
+let spesaNetta = spesa.toFixed(2);
+
+document.getElementById("output").innerHTML = spesaNetta;
+
